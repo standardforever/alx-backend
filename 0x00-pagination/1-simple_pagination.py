e5140c3e9b46ee8ecc6cd12c9967ffd7adee56f8
standardforver <standard.forver123@gmail.com>
2023-02-28 01:05:01 -0500

    hashband added

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+simple pagination
+"""
+
 import csv
 import math
 from typing import List, Tuple
--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+hypermedia pagination
+"""
+
 import csv
 import math
 from typing import List, Tuple
